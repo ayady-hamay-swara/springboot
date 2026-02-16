db9ea1059d165f5aa3a55305fe149279bc055303
ayady-hamay-swara <ayad.243439@stu.univsul.edu.iq>
2026-02-16 11:29:26 +0300

    Removed folder from repo and added to gitignore

--- 1-springwebmvcfinal/src/main/java/lk/ijse/dep/webmvc/entity/SuperEntity.java
+++
@@ -1,6 +0,0 @@
-package lk.ijse.dep.webmvc.entity;
-
-import java.io.Serializable;
-
-public class SuperEntity implements Serializable {
-}
--- 1-springwebmvcfinal/src/main/java/lk/ijse/dep/webmvc/services/SuperService.java
+++
@@ -1,4 +0,0 @@
-package lk.ijse.dep.webmvc.services;
-
-public interface SuperService {
-}
